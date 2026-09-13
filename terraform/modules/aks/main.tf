224f3d3c72c22d7d3d86778f91ae3a268379f164
zolij <zolij86@gmail.com>
2026-09-13 16:00:52 +0200

    refactor: standardize formatting in default_node_pool block

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -17,13 +17,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   sku_tier = var.environment == "prod" ? "Standard" : "Free"
 
   default_node_pool {
-    name                 = "systempool"
-    node_count           = var.node_count
-    vm_size              = var.vm_size
-    vnet_subnet_id       = var.subnet_id
-    os_disk_size_gb      = 30
-    max_pods             = 50
-    enable_host_encryption = true
+    name                         = "systempool"
+    node_count                   = var.node_count
+    vm_size                      = var.vm_size
+    vnet_subnet_id               = var.subnet_id
+    os_disk_size_gb              = 30
+    max_pods                     = 50
+    enable_host_encryption       = true
     only_critical_addons_enabled = false # Set true in production when dedicated user node pool is present
   }
 
