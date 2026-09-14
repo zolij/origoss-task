1df130fa3ab66727813f0bf874d37712430feb4f
zolij <zolij86@gmail.com>
2026-09-14 15:56:32 +0200

    fix: correct helm provider configuration syntax in main.tf and add versions.tf for platform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,10 +54,10 @@ module "aks" {
 
 provider "helm" {
   kubernetes = {
-    host                   = module.aks.kube_config.0.host
-    client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
-    client_key             = base64decode(module.aks.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+    host                   = module.aks.kube_config[0]host
+    client_certificate     = base64decode(module.aks.kube_config[0]client_certificate)
+    client_key             = base64decode(module.aks.kube_config[0]client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config[0]cluster_ca_certificate)
   }
 }
 
---
+++ terraform/modules/platform/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
