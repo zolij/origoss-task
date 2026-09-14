9cd688355ca121b5a2c3f037101982e0be6603de
zolij <zolij86@gmail.com>
2026-09-14 15:59:50 +0200

    fix: remove unnecessary terraform block and correct helm provider configuration syntax in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.90"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
@@ -54,10 +44,10 @@ module "aks" {
 
 provider "helm" {
   kubernetes = {
-    host                   = module.aks.kube_config[0]host
-    client_certificate     = base64decode(module.aks.kube_config[0]client_certificate)
-    client_key             = base64decode(module.aks.kube_config[0]client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config[0]cluster_ca_certificate)
+    host                   = module.aks.kube_config[0].host
+    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(module.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
   }
 }
 
