40e4d14ccf4e88ddf96eecc63b19a9d3d154cd6b
zolij <zolij86@gmail.com>
2026-09-14 15:52:41 +0200

    fix: update output descriptions in outputs.tf for clarity

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -1,14 +1,21 @@
-output "cluster_name" {
-  value       = azurerm_kubernetes_cluster.aks.name
-  description = "Name of the AKS cluster"
-}
-
 output "cluster_id" {
+  description = "The ID of the AKS cluster."
   value       = azurerm_kubernetes_cluster.aks.id
-  description = "ID of the AKS cluster"
 }
 
-output "kubelet_identity_object_id" {
-  value       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  description = "Object ID of the Kubelet Managed Identity"
+output "cluster_name" {
+  description = "The name of the AKS cluster."
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kube_config" {
+  description = "Raw Kubernetes configuration credentials."
+  value       = azurerm_kubernetes_cluster.aks.kube_config
+  sensitive   = true
+}
+
+output "kube_config_raw" {
+  description = "Raw kubeconfig string."
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive   = true
 }
